3204d2fc87bf8cc1a62c781afa3cde74f5e3a8bc
Sang <92257684+shchoil@users.noreply.github.com>
2022-02-26 00:44:42 +0900

    이상훈 추가/피드백DAO/feedbacoDAO

--- semiproject_team1/src/main/java/com/semi/dao/feedbackDAO.java
+++ semiproject_team1/src/main/java/com/semi/dao/feedbackDAO.java
@@ -1,7 +1,11 @@
 package com.semi.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import com.semi.dto.Feedback;
 
 @Mapper
 @Repository
