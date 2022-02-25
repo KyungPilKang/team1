96090806827c6350c645845c35f3397818de2e98
KyungPilKang <97011445+KyungPilKang@users.noreply.github.com>
2022-02-25 16:11:50 +0900

    경필/추가 : RiotAPI/연동아이디 전적가져오기 기능구현

--- semiproject_team1/src/main/java/com/semi/controller/MypageController.java
+++ semiproject_team1/src/main/java/com/semi/controller/MypageController.java
@@ -1,8 +1,34 @@
 package com.semi.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.semi.api.riotapi.RiotAPI;
 
 @Controller
 public class MypageController {
-
+	@Autowired
+	RiotAPI riotapi;
+		
+	
+	@ResponseBody
+	@GetMapping("/statustest")
+	public String statustest(@RequestParam(value="mem_link_id")String mem_link_id) {
+		String result;
+		Map<String, String> map=new HashMap<>();
+		map=riotapi.status(riotapi.getIdCode(mem_link_id));
+		result="티어: "+map.get("tier")+"("+map.get("rank")+")"+" 승: "+map.get("wins")+" 패: "+map.get("losses");
+		return result;
+	}
+	
 }
