a2b2662160880cd037dada8c49adb6f2ce3b73e4
KyungPilKang <97011445+KyungPilKang@users.noreply.github.com>
2022-02-25 18:55:12 +0900

    경필/추가 : RiotAPI 기능구현/모스트챔피언 데이터핸들링 클래스추가(구현중)

--- semiproject_team1/src/main/java/com/semi/api/riotapi/RiotAPI.java
+++ semiproject_team1/src/main/java/com/semi/api/riotapi/RiotAPI.java
@@ -92,4 +92,44 @@ public class RiotAPI {
 		}
 		return map;
 	}
+	
+	public Map<Integer, String> mostChampion(String id){
+		Map<Integer, String> map=new HashMap<>();
+		StringBuffer response = new StringBuffer();
+		try {
+			String apiURL = "https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/"+id;
+			URL url = new URL(apiURL);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36");
+			con.setRequestProperty("Accept-Language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7");
+			con.setRequestProperty("Accept-Charset", "application/x-www-form-urlencoded; charset=UTF-8");
+			con.setRequestProperty("Origin", "https://developer.riotgames.com");
+			con.setRequestProperty("X-Riot-Token", APIKEY);
+			
+			int responseCode = con.getResponseCode();
+			BufferedReader br;
+			if (responseCode == 200) {
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else {
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+			String inputLine;
+
+			while ((inputLine = br.readLine()) != null) {
+				response.append(inputLine);
+			}
+			br.close();
+			JSONParser temp=new JSONParser();
+			JSONArray resultarr=(JSONArray) temp.parse(response.toString());
+			for(int i=0;i<3;i++) {
+				JSONObject resultobj=(JSONObject)resultarr.get(i);
+				map.put(i,String.valueOf(resultobj.get("championId")));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return map;
+	}
 }
--- semiproject_team1/src/main/java/com/semi/controller/MypageController.java
+++ semiproject_team1/src/main/java/com/semi/controller/MypageController.java
@@ -1,9 +1,5 @@
 package com.semi.controller;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,12 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.semi.api.riotapi.LOL_champion;
 import com.semi.api.riotapi.RiotAPI;
 
 @Controller
 public class MypageController {
 	@Autowired
 	RiotAPI riotapi;
+	
+	@Autowired
+	LOL_champion champion;
 		
 	
 	@ResponseBody
@@ -27,8 +27,18 @@ public class MypageController {
 		String result;
 		Map<String, String> map=new HashMap<>();
 		map=riotapi.status(riotapi.getIdCode(mem_link_id));
+		System.out.println(map);
 		result="티어: "+map.get("tier")+"("+map.get("rank")+")"+" 승: "+map.get("wins")+" 패: "+map.get("losses");
 		return result;
+	}
+	
+	@ResponseBody
+	@GetMapping("/mosttest")
+	public String mosttest(@RequestParam(value="mem_link_id")String mem_link_id) {
+		Map<Integer, String> map=new HashMap<>();
+		map=riotapi.mostChampion(riotapi.getIdCode(mem_link_id));
+		System.out.println(map);
+		return champion.getChampionName(map.get(0));
 	}
 	
 }
