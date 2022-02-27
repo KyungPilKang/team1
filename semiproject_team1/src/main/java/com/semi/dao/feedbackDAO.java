e1cc8f34a00ff381211e823f3a825f2da6391a82
Sang <92257684+shchoil@users.noreply.github.com>
2022-02-27 22:44:17 +0900

    lsh/feedbak/mapper,dao작성

--- semiproject_team1/src/main/java/com/semi/dao/feedbackDAO.java
+++ semiproject_team1/src/main/java/com/semi/dao/feedbackDAO.java
@@ -1,11 +1,21 @@
 package com.semi.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import com.semi.dto.Feedback;
 
 @Mapper
 @Repository
 public interface feedbackDAO {
 	
-
+	void insertFeedback(Feedback feedback) throws Exception;
+	Integer selectMaxFeedbackNum() throws Exception;
+	
+	int selectFeedbackCount() throws Exception;
+	
+	List<Feedback> selectFeedbackList_all(int startrow) throws Exception;
+	List<Feedback> selectFeedbackList_all_viewsSort(int startrow) throws Exception;
 }
