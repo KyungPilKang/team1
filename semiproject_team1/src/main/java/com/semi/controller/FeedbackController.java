b91d67970e7aedbc2c162b9ce2859d36a2395639
Sang <92257684+shchoil@users.noreply.github.com>
2022-03-02 14:12:28 +0900

    lsh/피드백폼 피드백요청폼 작성

--- semiproject_team1/src/main/java/com/semi/controller/FeedbackController.java
+++ semiproject_team1/src/main/java/com/semi/controller/FeedbackController.java
@@ -1,8 +1,18 @@
 package com.semi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FeedbackController {
-
+	
+	 @GetMapping("/feedbackform_all")
+	    public String feedbackform_all() {
+	        return "feedbackForm_all";
+	    }
+	 
+	 @GetMapping("/feedbackwriteform")
+	 	public String feedbackwriteform() {
+		 return "feedbackwriteForm";
+	 }
 }
