67f6c0b30c1e1f75c3ca493ffd668775a4605142
Sang <92257684+shchoil@users.noreply.github.com>
2022-03-12 20:04:41 +0900

    lsh/main페이지 시작/main1.css(jsp)추가

--- semiproject_team1/src/main/java/com/semi/controller/MainpageController.java
+++ semiproject_team1/src/main/java/com/semi/controller/MainpageController.java
@@ -1,10 +1,14 @@
 package com.semi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 //메인 페이지 컨트롤러
 @Controller
 public class MainpageController {
 
-	
+	@GetMapping("/main1")
+	public String main1() {
+		return "main/main1";
+	}
 }
